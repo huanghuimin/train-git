9d1aa2959df564bce0b64aaa33c0ced49335954b
huanghm <huanghm@geovis.cn>
2019-07-17 17:46:34 +0800

    update

---
+++ hello.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello, World!");
+
+  for i := 0; i<20; i++ {
+    fmt.Print("*");
+  }
+  fmt.Println();
+  fmt.Println("* this my first");
+
+  for i := 0; i<20; i++ {
+     fmt.Print("*");
+  }
+  fmt.Println();
+
+}
